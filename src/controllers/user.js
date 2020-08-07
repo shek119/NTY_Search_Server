c39d1b6e897e49a11dc62ea1349a891297a0bd07
wtshek <44945470+wtshek@users.noreply.github.com>
2020-08-07 10:27:28 +0200

    update save article function

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -2,10 +2,35 @@ const User = require("../models/user");
 
 const saveArticle = (req, res) => {
   const { id, url } = req.body;
-  console.log(req.body);
-  User.findById(id, (err, user) => {
-    console.log(user);
-  });
+
+  // User.findById(id, { articles: { $exists: false } }, () => {
+  //   console.log("articles field does not exist");
+
+  //   User.updateOne(
+  //     { _id: id },
+  //     { $set: { articles: [] } },
+  //     { upsert: true },
+  //     function (err) {
+  //       console.log(err);
+  //     }
+  //   );
+  // });
+
+  // console.log(url);
+
+  User.findByIdAndUpdate(
+    id,
+    { $push: { articles: url } },
+    { new: true },
+    (err, user) => {
+      if (err)
+        res
+          .status(400)
+          .send({ message: "internal server error. Please try again" });
+
+      res.status(200);
+    }
+  );
 };
 
 module.exports = { saveArticle };
